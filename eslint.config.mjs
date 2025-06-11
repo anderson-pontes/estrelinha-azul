adcca8111266950891b89ecfa35dcf66b2c9ca0b
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-06-10 21:50:50 -0300

    ajuste eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+ 
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
@@ -18,5 +14,5 @@ const eslintConfig = [
     },
   }),
 ]
-
-export default eslintConfig;
+ 
+export default eslintConfig
