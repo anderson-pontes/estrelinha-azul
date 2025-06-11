0536951401cda259496ee8b23e2d19d976a680fd
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-06-10 22:11:45 -0300

    ajuste de componente

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
 import { FlatCompat } from '@eslint/eslintrc'
- 
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 })
- 
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next'],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+
+      // 👇 Adicione isso:
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
     },
   }),
 ]
- 
+
 export default eslintConfig
